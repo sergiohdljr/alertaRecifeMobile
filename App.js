57d933f6d78e88810349dbe4dc14a515d6767ab0
sergiohdljr <sergio.tjf1@gmail.com>
2023-06-03 11:08:16 -0300

    feat: themes app

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { temaDark } from "./src/styles/theme/darkTheme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={temaDark}>
+      <View style={styles.container}>
+        <TextStyle>test</TextStyle>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/styles/theme/lightTheme.js
@@ -0,0 +1,11 @@
+export const temaLight = {
+    tema: "light",
+  
+    cores: {
+      background: "#E1CCFF",
+      app: "#FFFF",
+      texto: "#0F1419",
+      textoSecundario: "#5B7083",
+      roxoEscuro: "#8B3DFF",
+    },
+  };
