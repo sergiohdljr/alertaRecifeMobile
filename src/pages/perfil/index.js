c1c7068bd02fbcc18acbf495880bef00dbc78a5b
scarlletValentim <scarlletrvs34@gmail.com>
2023-06-07 19:10:42 -0300

    perfil

--- src/pages/perfil/index.js
+++ src/pages/perfil/index.js
@@ -1,11 +1,76 @@
-import { View, SafeAreaView, TouchableOpacity, Text } from "react-native";
+import { View, SafeAreaView, TouchableOpacity, Text , Image, StyleSheet} from "react-native";
 import { List } from "phosphor-react-native";
 
 const Perfil = () => {
   return (
-    <SafeAreaView style={{ flex: 1, padding: 20 }}>
-      <Text style={{ fontSize: 12 }}> Perfil</Text>
+    <SafeAreaView style={styles.container}>
+    <View style={styles.perfilHead}>
+<Image style={styles.image}></Image> 
+
+<Text style={styles.textNome}>Scarllet Valentim</Text>
+<Text style={styles.textUser}>@scarlletrvs34</Text>
+<Text style={styles.textOcorrencias}>
+  Suas Ocorrências
+</Text>
+
+
+    </View>
+
+    
     </SafeAreaView>
   );
 };
+const styles= StyleSheet.create({
+  container:{
+    flex:1,
+    display:'flex',
+    padding: 10 ,
+     backgroundColor:'#a0a0a0',
+     flexDirection:'column',
+     justifyContent:'flex-start'
+    
+  },
+  perfilHead:{
+   paddingVertical:20,
+   backgroundColor:'#fff',
+    shadowRadius: 4,
+    shadowColor: "rgba(0,0,0,0.6)",
+    shadowOffset: { width: 2, height: 2 },
+    elevation: 6,
+    borderBottomLeftRadius:8,
+    borderBottomRightRadius:8
+
+   
+  },
+ 
+  image:{  
+    width: 95,
+    height: 95,
+    borderRadius: '50%',
+    marginTop: 3,
+    marginLeft: 18,
+    border: "2px solid black",
+
+  },
+  textNome:{
+    paddingLeft:10,
+    fontSize:17,
+    fontWeight:'bold',
+    paddingTop:6
+  },
+  textUser:{
+    paddingLeft:10,
+    fontSize:12,
+    fontWeight:'bold',
+    color:'#a0a0a0'
+  },
+  textOcorrencias:{
+    paddingLeft:10,
+    fontSize:20,
+    fontWeight:'bold',
+    marginTop:25,
+    letterSpacing:2,
+    color:'#8B3DFFFF'
+  }
+})
 export default Perfil;
