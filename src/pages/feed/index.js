54d09c82940f28c66c7f1d9689560f9ecd129d9a
sergiohdljr <sergio.tjf1@gmail.com>
2023-06-10 10:30:14 -0300

    fetch ocorrencias feed

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,4 +1,4 @@
-import  {useState} from "react";
+import { useState, useEffect } from "react";
 import {
   View,
   SafeAreaView,
@@ -6,126 +6,147 @@ import {
   Text,
   StyleSheet,
   Image,
-  TextInput, 
-  Picker, 
-  Modal
+  TextInput,
+  Picker,
+  Modal,
 } from "react-native";
 import { BellRinging } from "phosphor-react-native";
 import { ActionModalEdit } from "../../components/modalEdit";
 import BarraPesquisa from "../../components/barraPesquisa";
+import { Post } from "../../components/post";
+import { api } from "../../service/api";
 
 const Feed = () => {
- 
-  const [visibleModalEdit, setVisibleModalEdit]= useState(false);
-  return (
+  const [visibleModalEdit, setVisibleModalEdit] = useState(false);
+  const [ocorrecia, setOcorrencia] = useState();
 
-    
-   
-    
-      <SafeAreaView style={{ flex: 1,  backgroundColor: "#a0a0a0a0" }}>
-        <BarraPesquisa/>
-        <View style={styles.postOcorrencia}>
-          <View style={styles.itensPost}>
-            <View style={{ display: "flex", flexDirection:'row'}}>
-              <Image style={styles.imagepost}></Image>
-              <TextInput  placeholder='Digite aqui sua ocorrência' style={styles.TextInputOcorrencia}></TextInput>
-            </View>
-            <View> 
-              <TextInput placeholder='Informe a localização' style={styles.TextInputLocalizacao} ></TextInput>
-            </View>
-            <View style={{flexDirection:'row', display:'flex', marginTop:10, gap:10, justifyContent:  "space-evenly"}}>
-  
-  
-            <Picker
+  useEffect(() => {
+    api
+      .get("/ocorrencias")
+      .then((ocorrencias) => setOcorrencia(ocorrencias.data));
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#a0a0a0a0" }}>
+      <BarraPesquisa />
+      {ocorrecia && console.log(ocorrecia)}
+      <View style={styles.postOcorrencia}>
+        <View style={styles.itensPost}>
+          <View style={{ display: "flex", flexDirection: "row" }}>
+            <Image style={styles.imagepost}></Image>
+            <TextInput
+              placeholder="Digite aqui sua ocorrência"
+              style={styles.TextInputOcorrencia}
+            ></TextInput>
+          </View>
+          <View>
+            <TextInput
+              placeholder="Informe a localização"
+              style={styles.TextInputLocalizacao}
+            ></TextInput>
+          </View>
+          <View
             style={{
-              height: 40,
-              width: 100,
-              borderRadius: 8,
-              fontWeight: "bold",
-              alignItems: "center",
-              alignSelf: "center",
+              flexDirection: "row",
               display: "flex",
-              marginBottom: 14,
-              justifyContent: "center",
-  
-              fontSize: 12,
-              shadowColor: "rgba(0,0,0,0.5)",
-              shadowOffset: { width: 0, height: 2 },
-              elevation: 5,
-              shadowRadius: 4,
-              marginLeft:50,
-              bordeSize:1,
-              borderColor:'#8D3DFFFF'
+              marginTop: 10,
+              gap: 10,
+              justifyContent: "space-evenly",
             }}
           >
-  
-            <Picker.Item label="Assalto" value="Assalto" />
-            <Picker.Item label="Assédio" value="Assédio" />
-            <Picker.Item label="Arrastão" value="Arrastão" />
-            <Picker.Item
-              label="Acidente de trânsito"
-              value="Acidente de trânsito"
-            />
-            <Picker.Item label="Agressão" value="Agressão" />
-            <Picker.Item label="Racismo" value="Racismo" />
-          </Picker>
-  
-  
-  
-          <TouchableOpacity onPress={()=> setVisibleModalEdit(true)}>
-            <View style={styles.actionButton}>
-              <BellRinging size={22} color="white" />
-              <Text
-                style={{
-                  fontSize: 17,
-                  fontWeight: "bold",
-                  color: "#fff",
-                  marginLeft: 4,
-                }}
-              >
-                Alertar
-              </Text>
-            </View>
-          </TouchableOpacity>
-  
-  
-  
-          <Modal 
-  visible={visibleModalEdit} 
-  transparent={true} 
-  onRequestClose={ ()=> setVisibleModalEdit(false)} >
-  <ActionModalEdit handleClose={()=>setVisibleModalEdit(false)}/>
-  
-  </Modal>
-  
-  
-  
+            <Picker
+              style={{
+                height: 40,
+                width: 100,
+                borderRadius: 8,
+                fontWeight: "bold",
+                alignItems: "center",
+                alignSelf: "center",
+                display: "flex",
+                marginBottom: 14,
+                justifyContent: "center",
 
-  
-            </View>
-        
+                fontSize: 12,
+                shadowColor: "rgba(0,0,0,0.5)",
+                shadowOffset: { width: 0, height: 2 },
+                elevation: 5,
+                shadowRadius: 4,
+                marginLeft: 50,
+                bordeSize: 1,
+                borderColor: "#8D3DFFFF",
+              }}
+            >
+              <Picker.Item label="Assalto" value="Assalto" />
+              <Picker.Item label="Assédio" value="Assédio" />
+              <Picker.Item label="Arrastão" value="Arrastão" />
+              <Picker.Item
+                label="Acidente de trânsito"
+                value="Acidente de trânsito"
+              />
+              <Picker.Item label="Agressão" value="Agressão" />
+              <Picker.Item label="Racismo" value="Racismo" />
+            </Picker>
+
+            <TouchableOpacity onPress={() => setVisibleModalEdit(true)}>
+              <View style={styles.actionButton}>
+                <BellRinging size={22} color="white" />
+                <Text
+                  style={{
+                    fontSize: 17,
+                    fontWeight: "bold",
+                    color: "#fff",
+                    marginLeft: 4,
+                  }}
+                >
+                  Alertar
+                </Text>
+              </View>
+            </TouchableOpacity>
+
+            <Modal
+              visible={visibleModalEdit}
+              transparent={true}
+              onRequestClose={() => setVisibleModalEdit(false)}
+            >
+              <ActionModalEdit handleClose={() => setVisibleModalEdit(false)} />
+            </Modal>
           </View>
         </View>
-      </SafeAreaView>
+      </View>
+      <View>
+        {ocorrecia &&
+          ocorrecia.map((ocorrencias) => {
+            return (
+              <Post
+                displayName={ocorrencias.autor.nome}
+                email={ocorrencias.autor.email}
+                photoURL={ocorrencias.autor.fotoPerfil}
+                tipoOcorrencia={ocorrencias.tipoDaOcorrencia}
+                descricaoDaOcorrencia={ocorrencias.descricaoDaOcorrencia}
+                enderecoOcorrencia={ocorrencias.enderecoOcorrencia}
+                key={ocorrencias.id}
+              />
+            );
+          })}
+      </View>
+    </SafeAreaView>
   );
 };
-
 
 const styles = StyleSheet.create({
   postOcorrencia: {
     width: "100%",
     height: 220,
-    borderBottomLeftRadius:8,
-    borderBottomRightRadius:8,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
     backgroundColor: "#fff",
-    paddingTop:6.5,
+    paddingTop: 6.5,
     shadowRadius: 4,
     shadowColor: "rgba(0,0,0,0.6)",
     shadowOffset: { width: 2, height: 2 },
     elevation: 6,
     paddingLeft: 5,
-    marginTop:2
-    
+    marginTop: 2,
   },
   imagepost: {
     border: "2px solid black",
@@ -153,55 +174,49 @@ const styles = StyleSheet.create({
     paddingTop: 16,
     paddingLeft: 9,
   },
-  TextInputOcorrencia:{
-    width: '80%',
-    height:100,
-    borderRadius:8,
-    border:'1px solid gray',
+  TextInputOcorrencia: {
+    width: "80%",
+    height: 100,
+    borderRadius: 8,
+    border: "1px solid gray",
     shadowRadius: 4,
     shadowColor: "rgba(0,0,0,0.6)",
     shadowOffset: { width: 2, height: 2 },
     elevation: 6,
-    marginLeft:8,
-    padding:2,
-    display:'flex',
-    alignItems:'flex-start',
-    justifyContent:'flex-start',
-    wordBreak:'break-all'
-
-    
+    marginLeft: 8,
+    padding: 2,
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    wordBreak: "break-all",
   },
-  TextInputLocalizacao:{
-    width: '80%',
-    height:35,
-    borderRadius:8,
-    border:'1px solid gray',
+  TextInputLocalizacao: {
+    width: "80%",
+    height: 35,
+    borderRadius: 8,
+    border: "1px solid gray",
     shadowRadius: 4,
     shadowColor: "rgba(0,0,0,0.6)",
     shadowOffset: { width: 2, height: 2 },
     elevation: 6,
-    marginLeft:62,
-    padding:2,
-    marginTop:5
+    marginLeft: 62,
+    padding: 2,
+    marginTop: 5,
   },
   actionButton: {
     backgroundColor: "#8B3DFFFF",
     borderRadius: 8,
-    width:130,
-    height:42,
+    width: 130,
+    height: 42,
     shadowColor: "rgba(0,0,0,0.6)",
     shadowOffset: { width: 0, height: 2 },
     elevation: 5,
     shadowRadius: 4,
-    justifyContent:'center',
-    display:'flex',
-    alignItems:'center',
-    flexDirection:'row',
-    marginLeft:28
-
+    justifyContent: "center",
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    marginLeft: 28,
   },
-  
-
-
 });
 export default Feed;
--- src/pages/perfil/index.js
+++ src/pages/perfil/index.js
@@ -6,56 +6,18 @@ import {
   Image,
   StyleSheet,
 } from "react-native";
-
-import { PencilSimpleLine, Trash , BellRinging } from "phosphor-react-native";
+import { Post } from "../../components/post";
 
 const Perfil = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.perfilHead}>
         <Image style={styles.image}></Image>
-
         <Text style={styles.textNome}>Scarllet Valentim</Text>
         <Text style={styles.textUser}>@scarlletrvs34</Text>
         <Text style={styles.textOcorrencias}>Suas Ocorrências</Text>
       </View>
-
-      <View style={styles.postOcorrencia}>
-        <View style={styles.postItens}>
-          <View
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              flexDirection: "row",
-              with: "100%",
-              alignItens:'center',
-              gap:8
-            }}
-          >
-            <View style={{display:'flex',flexGrow:1}}>
-              <Image style={styles.imagePost}></Image>
-            </View>
-            <BellRinging size={22} color="red"/> 
-
-            <View style={{marginRight:5}}>
-
-            <Text style={{color:'red', fontSize:12, fontWeight:'bold'}}>Assalto!</Text>
-            </View>
-            <PencilSimpleLine size={24} color="blue" />
-            <Trash size={24} color="red" />
-            
-
-         
-          </View>
-          
-          <View style={styles.textUsuario}>
-            <Text style={styles.textNomePost}>Scarllet Valentim</Text>
-            <Text style={styles.textUserPost}>@scarlletrvs34</Text>
-          </View>
-          <Text style={styles.TextOcorrenciaDescricao}>Roubo de caneta!!</Text>
-          <Text style={styles.localizacao}>Localização</Text>
-        </View>
-      </View>
+      <Post />
     </SafeAreaView>
   );
 };
@@ -107,58 +69,5 @@ const styles = StyleSheet.create({
     letterSpacing: 2,
     color: "#8B3DFFFF",
   },
-  postOcorrencia: {
-    marginTop: 6.5,
-    height: 220,
-    paddingTop: 10,
-    backgroundColor: "#fff",
-    borderRadius: 8,
-    shadowRadius: 4,
-    shadowColor: "rgba(0,0,0,0.6)",
-    shadowOffset: { width: 2, height: 2 },
-    elevation: 6,
-    display: "flex",
-    padding: 9,
-    paddingLeft: 10,
-    justifyContent: "flex-start",
-  },
-  imagePost: {
-    width: 50,
-    height: 50,
-    borderRadius: "50%",
-    border: "2px solid black",
-    display: "flex",
-    justifyContent: "flex-start",
-
-  },
-  postItens: {
-    display: "flex",
-    justifyContent: "flex-start",
-  },
-  textNomePost: {
-    fontSize: 12,
-    fontWeight: "bold",
-    paddingTop: 8,
-  },
-  textUserPost: {
-    fontSize: 10,
-    fontWeight: "bold",
-    color: "#a0a0a0",
-  },
-  textUsuario: {
-    display: "flex",
-  },
-  TextOcorrenciaDescricao: {
-    fontSize: 13,
-    fontWeight: "bold",
-    paddingTop: 8,
-    paddingLeft: 5,
-  },
-  localizacao:{
-    fontSize: 14,
-    fontWeight: "bold",
-    color: "#a0a0a0",
-    marginTop:17
-  }
 });
 export default Perfil;
